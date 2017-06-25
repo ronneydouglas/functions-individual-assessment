81b9f84219b4da59b74affa0718403661e20c06a
Ronney <Ronney@Loomer.local>
2017-06-24 22:57:26 -0400

    hopefully

--- main.js
+++ main.js
@@ -2,10 +2,12 @@
 // Define a function max() that takes two numbers as arguments and returns the largest of them. Use the if-then-else construct available in JavaScript.
 // A:
 
-function max(num1, num2){
-    if (num1 > num2);
-    return num1;
-    else return num2;
+function max(num1, num2) {
+    if (num1 > num2) {
+    return (num1);
+}
+    else {
+    return (num2);
 }
 
 
@@ -13,31 +15,37 @@ function max(num1, num2){
 // Define a function maxOfThree() that takes three numbers as arguments and returns the largest of them.
 // A:
 
-function maxOfThree(num1, num2, num3){
-    if (num1 > num2 && num3);
+function maxOfThree(num1, num2, num3) {
+    if (num1 > num2 && num3) {
     return num1;
-    if (num2 > num21 && num3);
-    return num21;
-    if (num3 > num1 && num2);
+  }
+    else if (num2 > num1 && num3) {
+    return num2;
+  }
+    else if (num3 > num1 && num2) {
     return num3;
 }
+
 
 
 // 3.
 // Write a function that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false otherwise.
 // A:
 
-function isVowel(char){
-    if (char == a,e,i,o,u);
-    return true;
-    else return false;// Your answer here
+function isVowel(char) {
+    if (char == a, e, i, o, u) {
+    return 'true';
+  }
+    else {
+    return 'false';
+  }
 }
 
 
 // 4.
 // Write a function called `sum` that takes two parameters and returns the sum of those 2 numbers.
 // A:
-function sum(num1,num2){
+function sum(num1, num2) {
     return (num1 + num2);
 }
 
@@ -46,13 +54,18 @@ function sum(num1,num2){
 // 5.
 // Write a function named `avg` that takes 3 parameters and returns the average of those 3 numbers.
 // A:
+function avg(num1, num2, num3) {
+    return (num1 + num2 + num3) / 3;
 
+}
 
 
 // 6.
 // Write a function called `getLength` that takes one parameter (a string) and returns the length
 // A:
-
+function getLength(wassup) {
+    return wassup.length;
+}
 
 
 // 7.
@@ -60,7 +73,14 @@ function sum(num1,num2){
 // and returns `true` if the second parameter is greater than the first.
 // Otherwise the function should return `false`.
 // A:
+function greaterThan(num1, num2) {
+    if(num2 > num1)
+    return true;
+} else {
+    return false
+}
 
+}
 
 
 // 8.
@@ -69,7 +89,9 @@ function sum(num1,num2){
 // is formated like "Hello, Name!" where *Name*
 // is the parameter that was passed in.
 // A:
-
+function greet(name) {
+    return "Hello, " + Name + "!";
+}
 
 
 // 9.
@@ -80,3 +102,11 @@ function sum(num1,num2){
 // words: "quick", "fox", "fence"
 // sentence: "quick brown fox jumps over the fence"
 // A:
+function madLib(word1, word2, word3, word4) {
+
+    var sentence = "People with " + word1 + " noses and " + word2 + " hands shouldn't " + word3 + " their own " + word4 + "!";
+
+    return sentence;
+}
+
+console.log(madLib("runny", "sweaty", "mix", "drinks"));
